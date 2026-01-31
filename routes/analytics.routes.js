07fd93bee2075c51459429a062fe9bb9b9858dc9
Ruksana Shaik <153373671+Ruksana0303@users.noreply.github.com>
2026-01-31 20:20:15 +0530

    Add analytics route to handle GET requests

---
+++ routes/analytics.routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { analytics } from '../controllers/analytics.controller.js'
+
+const router = express.Router()
+
+router.get('/', analytics)
+
+export default router
